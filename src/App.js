7ca981a2ef2575b068921d251ccf50fe0bf2044b
Tanner Langan <t.langan.07@gmail.com>
2021-10-16 13:47:32 -0700

    initial commit, adding base layout of budget calculator with static data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses/Expenses";
+import "./components//Expenses/Expenses.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "01",
+      title: "Car Insurance",
+      amount: 308.27,
+      date: new Date(2021, 2, 28),
+    },
+    { 
+      id: "02", 
+      title: "Rent",
+      amount: 400,
+      date: new Date(2021, 2, 28) },
+    {
+      id: "03",
+      title: "Phone Bill",
+      amount: 102.33,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "03",
+      title: "Dog",
+      amount: 590.00,
+      date: new Date(2021, 10, 22),
+    },
+  ];
 
-    const expenses = [
-        {id: '01', title: 'Car Insurance', amount: 308.27, date: new Date(2021, 2, 28)},
-        {id: '02', title: 'Rent', amount: 400, date: new Date(2021, 2, 28)},
-        {id: '03', title: 'Phone Bill', amount: 102.33, date: new Date(2021, 2, 28)},
-    ];
   return (
     <div>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+
+      <Expenses expenses = {expenses}></Expenses>
+        
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import React, { useState } from "react";
+
+function ExpenseItem(props) {
+
+
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler(){
+    
+     setTitle("Cat");
+
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <section className="expense-item__description">
+        <h2>{title}</h2>
+        <h2 className="expense-item__price">${props.amount}</h2>
+      </section>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
